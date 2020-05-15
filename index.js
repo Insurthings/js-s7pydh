d24b5de7eb033d3486d016532028bffd345d018c
Insurthings <insurseries@insurseries.tk>
2020-05-15 14:19:05 +0200

    1 binaries uploaded

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-// Import stylesheets
+/*// Import stylesheets
 import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = "<h1><center>Insur Series</center></h1>";
 appDiv.innerHTML = appDiv.innerHTML + "<p><center>Entretenimiento</p1></center>";
-var f = new File([""], "index.html", {type: "text/plain",});
+var f = new File([""], "index.txt", {type: "text/plain",});
 var t = new FileReader;
-appDiv.innerHTML = t.readAsText(f);
+t.readAsText(f);
+t.onload = function(html) {
+appDiv.innerHTML = html.target.result;
+
+};
+*/
